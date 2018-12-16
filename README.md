ed597eed49f792354e8b9889f001a3f2f5aa24d3
Fernando Montoya <montogeek@gmail.com>
2018-12-16 12:47:28 +0100

    Added support to js-with-links-details custom language

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 # remark-refractor
 
 Highlight code blocks in Markdown files with [Refractor](https://github.com/wooorm/refractor)
+
+Also add support for custom language called `js-with-links-details` via [MDX](https://mdxjs.com/) that allows you to write rich annotations inside JS snippets:
+
+// docs.mdx
+
+```js-with-links-details
+module.exports = {
+  <mode "/concepts/mode">
+    <default>
+      mode: "production", // "production" | "development" | "none"
+    </default>
+    mode: "production", // enable many optimizations for production builds
+    mode: "development", // enabled useful tools for development
+    mode: "none", // no defaults
+  </mode>
+}
+```
+
+You would need to add MDX loader to handle `.mdx` files to your favorite JS bundler.
+
+Once imported you would need to pass a `Pre` React component to the MDX `components` [prop](https://mdxjs.com/getting-started/#mdxprovider):
+
+```
+export default {
+  components: {
+    pre: Pre
+  }
+};
+```
+
+`Pre` component would receive your code as `children` prop, this plugin also adds `componentname` and `url` to every child, that you can use to build any imaginable UI:
+
+```js
+const components = {
+  mode: (children, props) => {
+    return <RichComponent children={children} {...props} />;
+  }
+};
+
+const Pre = props => {
+  const newChildren = React.Children.map(props.children.props.children, child => {
+    if (React.isValidElement(child)) {
+      if (child.props.props.className.includes("keyword")) {
+        return components[child.props.props.componentname];
+      }
+    }
+
+    return child;
+  });
+
+  const newProps = {
+    children: newChildren
+  };
+
+  return (
+    <pre>
+      <code {...newProps} />
+    </pre>
+  );
+};
+```
--- src/index.js
+++ src/index.js
@@ -9,34 +9,51 @@ refractor.register(require('refractor/lang/json.js'));
 refractor.register(require('refractor/lang/typescript.js'));
 refractor.register(require('refractor/lang/nginx.js'));
 refractor.register(require('refractor/lang/ruby.js'));
+refractor.register(require("./js-links-details.js"));
 
-module.exports = function attacher({ include, exclude } = {}) {
-  function visitor(node) {
-    const { lang } = node;
+function attacher({ include, exclude } = {}) {
+  return transformer;
 
-    if (!lang || (include && !~include.indexOf(lang)) || (exclude && ~exclude.indexOf(lang))) {
-      return;
-    }
+  function transformer(tree, file) {
+    visit(tree, "code", visitor);
 
-    let { data } = node;
+    function visitor(node) {
+      const { lang } = node;
 
-    if (!data) {
-      node.data = data = {};
-    }
-
-    try {
-      data.hChildren = refractor.highlight(node.value, lang);
-    } catch (e) {
-      if (!languages[lang]) {
-        console.warn('Prism does not support this language: ', lang);
-      } else {
-        console.warn('Prism failed to highlight: ', e);
+      if (!lang || (include && !~include.indexOf(lang)) || (exclude && ~exclude.indexOf(lang))) {
+        return;
       }
+
+      let { data } = node;
+
+      if (!data) {
+        node.data = data = {};
+      }
+
+      try {
+        data.hChildren = refractor.highlight(node.value, lang);
+        data.hChildren.forEach(node => {
+          if (node.properties && node.properties.className.includes("keyword")) {
+            if (node.children[1]) {
+              node.properties.componentname = node.children[1].value.trim();
+            }
+            if (node.children[2]) {
+              node.properties.url = node.children[2].children[0].value.replace(/"/g, "");
+            }
+          }
+        });
+      } catch (e) {
+        if (!languages[lang]) {
+          console.warn('Prism does not support this language: ', lang);
+        } else {
+          console.warn('Prism failed to highlight: ', e);
+        }
+      }
+
+      data.hProperties = data.hProperties || {};
+      data.hProperties.className = ["hljs", ...(data.hProperties.className || []), `language-${lang}`];
     }
-
-    data.hProperties = data.hProperties || {};
-    data.hProperties.className = ['hljs', ...(data.hProperties.className || []), `language-${lang}`];
   }
+}
 
-  return ast => visit(ast, 'code', visitor);
-};
+module.exports = attacher;
